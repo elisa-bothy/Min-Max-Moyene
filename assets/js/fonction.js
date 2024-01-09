ec78bbe1db15401aabef6261ceb0d3566510b202
Elisa Bothy <elisa.bothy@gmail.com>
2024-01-09 10:01:00 +0100

    version avant correction

--- assets/js/fonction.js
+++ assets/js/fonction.js
@@ -12,5 +12,36 @@ function min(){
     }
 }
 
+function max(){
+    const args = [...arguments];
+    if (args.length == 0)
+        return NaN;
+    else{
+        let maxtmp = args[0];
+        for(let i in args){
+            if (maxtmp < args[i])
+                maxtmp = args[i];
+        }
+        return maxtmp;
+    }
+}
+
+function moyenne(){
+    const args = [...arguments];
+    if (args.length == 0)
+        return NaN;
+    else{
+        let sommetmp = 0;
+        for(let i in args){
+            sommetmp += args[i];
+        }
+        return sommetmp/args.length;
+    }
+}
+
 console.log(min(5, 3, -8, 1, 25, 12));
 console.log(min());
+console.log(max(5, 3, -8, 1, 25, 12));
+console.log(max());
+console.log(moyenne(5, 3, -8, 1, 25, 12));
+console.log(moyenne ());
